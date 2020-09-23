b09bb6f7cb5239dea58ce5f893b3621e1eff7c07
Kaitlyn VanHook <kaitlynvanhook@Kaitlyns-MacBook-Pro-2.local>
2020-09-22 21:28:49 -0500

    can delete cows and farmers

--- src/javascripts/helpers/viewHelpers.js
+++ src/javascripts/helpers/viewHelpers.js
@@ -1,5 +1,6 @@
 import farmersView from '../components/views/farmersView';
 import cowsView from '../components/views/cowsView';
+import addCowView from '../components/views/addCowView';
 
 const viewHelper = (id) => {
   switch (id) {
@@ -7,12 +8,15 @@ const viewHelper = (id) => {
       return farmersView.farmersView();
     case 'cows-link':
       return cowsView.cowsView();
+    case 'add-cow-link':
+      return addCowView.addCowView();
     default:
       return console.warn('nothing clicked');
   }
 };
 
-const viewListener = () => {
+const viewListener = (view) => {
+  viewHelper(view);
   $('body').on('click', 'li.nav-item', (e) => {
     viewHelper(e.currentTarget.id);
   });
